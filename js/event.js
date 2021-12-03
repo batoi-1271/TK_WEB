55830982dae4bb9b7334b51c181bfbf0ab87f1f7
Ba Toi Nguyen <batoi1271.doe@gmail.com>
2021-12-03 10:55:58 +0700

    shop

---
+++ js/event.js
@@ -0,0 +1,34 @@
+$(window).scroll(function () {
+    let oTop = $('.grids-2').offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        $('.header').addClass('sticky');
+    } else {
+        $('.header').removeClass('sticky');
+    }
+})
+
+// animate gioi thieu sach
+
+let nCount = selector => {
+    $(selector).each(function () {
+        $(this).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: "swing",
+
+            step: function (value) {
+                $(this).text(Math.ceil(value))
+            }
+        })
+    })
+}
+
+let position = 0;
+$(window).scroll(function () {
+    let oTop = $('.numbers').offset().top - window.innerHeight
+    if(position == 0 && $(window).scrollTop() >= oTop) {
+        position++;
+        nCount(".rect > h1");
+    }
+})
